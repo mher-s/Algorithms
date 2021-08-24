9d117e260403fec4eaf1a4fe924172becf7cc726
Mher <mhersargsyan2001@gmail.com>
2021-08-24 20:39:27 +0400

    feat(selectionSort.js): selection sort implementetion

---
+++ SelectionSort/selectionSort.js
@@ -0,0 +1,20 @@
+const numbers = [10, 9, 2, 5, 0, 7, 4, 6, 1, 8, 3];
+
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let min = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[min] > arr[j]) {
+        min = j;
+      }
+    }
+    if (min !== i) {
+      let tmp = arr[i];
+      arr[i] = arr[min];
+      arr[min] = tmp;
+    }
+  }
+  return arr;
+}
+
+console.log(selectionSort(numbers));
