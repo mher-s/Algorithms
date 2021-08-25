2cf6af7824427d9303571389a873d7a603f99d2b
Mher <mhersargsyan2001@gmail.com>
2021-08-25 18:50:35 +0400

    feat(bubbleSort.js): bubble sort implementation

---
+++ BubbleSort/bubbleSort.js
@@ -0,0 +1,17 @@
+const numbers = [10, 9, 2, 5, 0, 7, 4, 6, 1, 8, 3];
+
+function bubbleSort(arr) {
+  let len = arr.length;
+  for (let i = len - 1; i >= 0; i--) {
+    for (let j = 1; j <= i; j++) {
+      if (arr[j - 1] > arr[j]) {
+        let temp = arr[j - 1];
+        arr[j - 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort(numbers));
