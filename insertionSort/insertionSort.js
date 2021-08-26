b8a2c38d92a2555a805e97c9b6ec0c7dfd1b7dd9
Mher <mhersargsyan2001@gmail.com>
2021-08-26 10:16:30 +0400

    feat(inerstionSort.js): implementetion

---
+++ insertionSort/insertionSort.js
@@ -0,0 +1,16 @@
+const numbers = [10, 9, 2, 5, 0, 7, 4, 6, 1, 8, 3];
+
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+      arr[j + 1] = arr[j];
+      j = j - 1;
+    }
+    arr[j + 1] = key;
+  }
+  return arr;
+}
+
+console.log(insertionSort(numbers));
